c89169e98859ea21ef657ee89be28f48efac823b
Brian O'Halloran <47759719+brianoh1979@users.noreply.github.com>
2024-04-10 18:37:16 +0100

    Adding some extra code comments

--- script.py
+++ script.py
@@ -1,20 +1,28 @@
 # import the necessary packages
+# library for https requests
 import requests
+# library for operating system dependent functionality such as read or write a file
 import os,time
+# Picamera Library is Raspberry Pi specfic and allows control of camera modules
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
 import cv2
 # initialize the Keras REST API endpoint URL along with the input
 # image path
+# initialize camera
 camera = PiCamera()
 camera.vflip=True
 rawCapture = PiRGBArray(camera)
 
+# Keras is Deep Learning module for Python. Here we are taking Keras model and deploying as a REST API (platform app). 
 KERAS_REST_API_URL = "https://objectdetection407.herokuapp.com/predict"
 IMAGE_PATH = "image.jpg"
 
+#Sleep for 5 seconds
 time.sleep(5)
+
+#input variables used within camera.capture method (function which belongs to object - PiCamera / camera in this case) for image and video.
 camera.capture(rawCapture, format="bgr")
 image = rawCapture.array
 print(image)
@@ -28,7 +36,7 @@ image = open(IMAGE_PATH, "rb").read()
 
 payload = {"image": image}
 
-# submit the request
+# submit the request and have the Keras Model analyze it via REST API call
 r = requests.post(KERAS_REST_API_URL, files=payload).json()
 
 # ensure the request was successful
